665ebd85acc42a29670dcd1fbf99f9ab0e59eb9e
deboralbupaiva <166133201+deboralbupaiva@users.noreply.github.com>
2024-08-16 12:09:09 -0300

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,29 @@
+console.log(`Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = true;
+const destino = "Curitiba";
+
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
+ 
+let contador = 0;
+let destinoExiste = false
+while(contador < 3){
+    if(listaDeDestinos[contador] == destino){
+        console.log("Destino existe");
+        destinoExiste = true;
+        break;
+    }
+    contador +=1;
+    
+} 
+console.log("Destino existe: ",destinoExiste);
